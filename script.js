a9c15f42270e36618a61a4874a789171d35cc2d0
unknown <raphafm.rf@gmail.com>
2023-10-09 22:00:18 -0300

    header + footer + bg

---
+++ script.js
@@ -0,0 +1,46 @@
+const primaryColor = '#458DCA';
+const secondaryColor = '#8B4513';
+const mainBg = '#190D04';
+
+let mainTag = document.getElementsByTagName("main").item(0);
+let mainChildren = mainTag.children;
+
+const resetNavbarColor = () => {
+  for(let i = 0; i < mainChildren.length; i++) {
+    document.querySelector(`header .${mainChildren[i].id}`).style['color'] = `${primaryColor}`;
+  }
+}
+
+const resetDisplay = (addlistener) => {
+  for(let i = 0; i < mainChildren.length; i++) {
+    if(mainChildren[i].id !== 'elementName')
+    mainChildren[i].style['display'] = 'none'
+
+    if(addlistener !== undefined) addlistener(mainChildren[i].id, mainChildren[i])
+  }
+}
+
+const addNavbarListener = (elementName, pageElem) => {
+  let element = document.querySelector(`header .${elementName}`);
+
+  element.addEventListener('click', () => {
+    resetNavbarColor()
+
+    element.style['color'] = `${secondaryColor}`
+
+    resetDisplay()
+    pageElem.style['display'] = 'flex'
+  });
+
+  // console.log(element);
+}
+
+// for(let i = 0; i < mainChildren.length; i++) {
+//   if(mainChildren[i].id !== 'Home') {
+//     mainChildren[i].style['display'] = 'none'
+//   }
+
+//   addNavbarListener(mainChildren[i].id, mainChildren[i])
+// }
+
+
